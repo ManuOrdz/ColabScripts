095c87a80336a95417acb00111660f068ff7b3c0
ManuOrdz <manuelordzslv@gmail.com>
2025-02-19 11:45:38 -0500

    add funtionality to download custom models

--- ollama_in_colab.sh
+++ ollama_in_colab.sh
@@ -1,14 +1,33 @@
 #!/bin/bash
 #Script to automatize the ollama install and pull some models
 
+#########################################
+#Description: Download model from ollama
+#Globals:
+# None
+#Arguments:
+# Model Name
+#Outputs:
+# List of models installed
+#Returns:
+# 0 if successful, none-zero on error
+#########################################
+function download_model() {
+  ollama pull $1
+}
+
 #Verify if ollama is install
 if ! which ollama >/dev/null; then
   echo "ollama could no be found"
+  echo "Installing ollama"
   curl -fsSL https://ollama.com/install.sh | sh
+  echo "Installing ollama successfull"
 else
   echo "ollama already installed"
   echo "running ollama"
   ollama serve &
-  ollama pull deepseek-r1:14b
-  ollama pull nomic-embed-text
+  echo ""
+  sleep 1
+  read -p "What model do you want to download: " model
+  download_model $model
 fi
